db18b28737b3a8f4683bd7145fe1a071bb04824b
Pwapou <valmiermathieu@me.com>
2014-07-09 21:34:07 +0200

    b

--- vue/v_user_index.php
+++ vue/v_user_index.php
@@ -99,32 +99,7 @@
                 </div>
                 <div class="row">
                 <!-- /.col-lg-8 -->
-                 <div class="col-lg-4">
-                    <div class="panel panel-primary">
-                        <div class="panel-body">
-                            <!-- The first class (green in this case) is assigned dynamically -->
-                                <div class="green clock"</div>
-
-                                <!-- This div holds the value of the unit monitored - seconds, minutes or hours -->
-                                <div class="display"></div>
-
-                                <!-- A black area that hides the underlying background -->
-                                <div class="front left"></div>
-
-                                <!-- The left part of the background: -->
-                                <div class="rotate left">
-                                <div class="bg left"></div>
-                                </div>
-
-                                <!-- The right part of the background: -->
-                                <div class="rotate right">
-                                <div class="bg right"></div>
-                                </div>
-
-                                </div>
-                        </div>
-                    </div>
-                </div>
+                 
                 <div class="col-lg-4">
                     <div class="panel panel-default">
                         <div class="panel-heading">
